b225227a75f53904c15650268c67e9ba6658147a
Jaspreet Randhawa <jaspreet.randhwa39@gmail.com>
2020-07-06 23:06:46 +1000

    minor updates

--- setUpMac.sh
+++ setUpMac.sh
@@ -6,11 +6,10 @@ if [[ $? != 0 ]] ; then
     ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)"
 else
     brew update
-    brewCheck="yes"
+fi
 
-if(brewCheck=="yes")
-#installs openJDK 11, git
-brew cask java
+#installs openJDK 11, git etc ....
+brew install java
 brew install git
 brew cask install atom
 brew cask install google-chrome
@@ -19,5 +18,3 @@ brew install ansible
 curl "https://awscli.amazonaws.com/AWSCLIV2.pkg" -o "AWSCLIV2.pkg"
 #as sudo  this requires a password.
 sudo installer -pkg ./AWSCLIV2.pkg -target /
-
-fi
